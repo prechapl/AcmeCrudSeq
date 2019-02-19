ba2e47e7c66c3a32b944ffc69d7c55cd2c07e916
Preston Chaplin <preston.chaplin@gmail.com>
2019-02-19 12:07:20 -0500

    deploy time

--- app.js
+++ app.js
@@ -1,13 +1,69 @@
 const express = require('express');
-const pg = require('postgres');
+const app = express();
+const { User } = require('./db')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const ejs = require('ejs');
 const methodOverride = require('method-override');
 
-const app = express();
+app.use('/public', express.static('public'));
+app.use(methodOverride('_method'));
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
 
-app.use('morgan');
-app.use('bodyParser');
-app.use('method-override');
+app.get('/', (req, res, next) => {
+    res.redirect('/users')
+});
+
+app.get('/users',  (req, res, next) => {
+    User.findAll()
+      .then(( users ) => res.render('index', { users }))
+      .catch(next)
+});
+
+app.get('/users/:id',  (req, res, next) => {
+  Promise.all([
+    User.findByPk(req.params.id),
+    User.findAll()
+    ])
+    .then(([ userToChange, users ]) => res.render('index-update', { userToChange, users }))
+    .catch(next)
+});
+
+app.post('/users', (req, res, next) => {
+  if (req.body.id) {
+    User.findOne({ where: { id: req.body.id } })
+      .then((userToChange) => userToChange.update({ name: req.body.name }))
+      .then(() => res.redirect(`/users`))
+      .catch(next)
+  } else {
+    User.create({ name: req.body.name })
+      .then(() => res.redirect(`/users`))
+      .catch(next)
+  }
+})
+
+app.put('/users/:id', (req, res, next) => {
+  Promise.all([
+    User.findAll(),
+    User.findOne({ where: { id: req.params.id }})
+  ])
+    .then(([ user, userToChange ]) => res.render('index', { user, userToChange }))
+    .then((user) => res.redirect(`/users/${user.id}`))
+    .catch(next)
+  })
+
+app.delete('/users/:id', (req, res, next) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+  })
+    .then(() => res.redirect('/users/'))
+    .catch(next);
+})
 
 module.exports = app;
+
--- db.js
+++ db.js
@@ -1,3 +1,24 @@
-const app = require('./app');
+const { app } = require('./app');
 const Sequelize = require('sequelize');
-const conn = new Sequelize('')
+const conn = new Sequelize(process.env.DATABASE_URL);
+// const conn = new Sequelize(process.env.DATABASE_URL, 'MacPro6', '', {dialect: 'postgres' });
+// const conn = new Sequelize('postgres://localhost/acme-crud-seq');
+
+const User = conn.define('user', {
+    name: Sequelize.STRING
+});
+
+const syncAndSeed = () => {
+  return conn.sync({ force: true})
+    // .then( async () => {
+
+    //   await Promise.all([
+    //     User.create({ name: 'Wagz'}),
+    //     User.create({ name: 'Ryder'}),
+    //     User.create({ name: 'Ziggy'})
+    //   ])
+    // })
+};
+
+module.exports = { User, syncAndSeed }
+
